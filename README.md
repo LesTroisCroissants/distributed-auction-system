1aac3108e61487382bed9bd70536f8c3837edb30
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-11-22 16:26:14 +0100

    add part of election process
    Co-authored-by: olivier <oliha@itu.dk>
    Co-authored-by: johan <johbr@itu.dk>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # distributed-auction-system
+
 Handin 5 of the introductory Distributed Systems course of ITU.
+
+servers are started with an id!
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"distributed-auction-system/proto"
 	"flag"
 	"fmt"
@@ -14,9 +15,12 @@ import (
 )
 
 var (
-	serverPorts       = [3]int{8080, 8081, 8082}
-	serverConnections = make([]proto.AuctionClient, 0)
-	port              = flag.Int("port", 8080, "port number")
+	serverPorts = [3]int{8080, 8081, 8082}
+	servers     = make(map[int]proto.AuctionClient) // map port to connections
+	serverId    = flag.Int("id", 1, "port number")
+	timeout     = 2 * time.Second //random assumption
+	leader      = 8080
+	bid         int32
 )
 
 type Server struct {
@@ -28,7 +32,7 @@ func main() {
 	flag.Parse()
 
 	server := &Server{
-		port: *port,
+		port: serverPorts[*serverId-1],
 	}
 
 	go startServer(server)
@@ -42,10 +46,40 @@ func main() {
 	time.Sleep(time.Hour)
 }
 
+func (server *Server) Election(ctx context.Context, currentCandidate *proto.RingLeaderTopDawgG) (*proto.Acknowledgement, error) {
+	//check for own identifier
+	if currentCandidate.ProcessID == int32(server.port) {
+		AnnounceResult(currentCandidate)
+		return &proto.Acknowledgement{}, nil
+	}
+
+	//compare with own bid and identifier
+	if currentCandidate.Bid < bid || (currentCandidate.Bid == bid && currentCandidate.ProcessID < int32(server.port)) {
+		currentCandidate.Bid = bid
+		currentCandidate.ProcessID = int32(server.port)
+	}
+
+	//pass along to neighbour
+	neighbour := servers[8080+((*serverId-1)%3)]
+	neighbour.Election(context.Background(), currentCandidate)
+
+	//wait for timeout
+
+	//pass further if timeout
+
+	return &proto.Acknowledgement{}, nil
+}
+
+func AnnounceResult(currentCandidate *proto.RingLeaderTopDawgG) {
+	for _, server := range servers {
+		server.Elected(context.Background(), currentCandidate)
+	}
+}
+
 func connectToServer(port int) {
 	conn, _ := grpc.Dial(":"+strconv.Itoa(port), grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	replica := proto.NewAuctionClient(conn)
-	serverConnections = append(serverConnections, replica)
+	servers[port] = replica
 
 	fmt.Println("Connected to port: ", strconv.Itoa(port))
 }
