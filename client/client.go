418493ae6481f604ff6dfe9dcace2f40dea14bdb
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-11-25 13:55:48 +0100

    add election code
    Co-authored-by: Johan <johbr@itu.dk>
    Co-authored-by: Olivier <oliha@itu.dk>

--- client/client.go
+++ client/client.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"context"
 	"distributed-auction-system/proto"
+	"fmt"
+	"strconv"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	server      proto.AuctionClient
+	serverPorts = [3]int{8080, 8081, 8082}
 )
 
 func main() {
+	connectToServer(serverPorts[0])
 
+	primary, _ := server.WhoIsNewLeader(context.Background(), &proto.Empty{}) // timeout not implemented
+
+	connectToServer(int(primary.Port))
+	server.Bid(context.Background(), &proto.ClientBid{Bid: 1})
 }
 
 type Server struct {
 	proto.UnimplementedAuctionServer
+}
+
+func connectToServer(port int) {
+	conn, _ := grpc.Dial(":"+strconv.Itoa(port), grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	server = proto.NewAuctionClient(conn)
+
+	fmt.Println("Connected to port: ", strconv.Itoa(port))
 }
--- server/server.go
+++ server/server.go
@@ -17,8 +17,9 @@ import (
 var (
 	serverPorts = [3]int{8080, 8081, 8082}
 	servers     = make(map[int]proto.AuctionClient) // map port to connections
-	serverId    = flag.Int("id", 1, "port number")
+	serverId    = flag.Int("id", 1, "server id")
 	timeout     = 2 * time.Second //random assumption
+	isElecting  = false
 	leader      = 8080
 	bid         int32
 )
@@ -47,33 +48,88 @@ func main() {
 }
 
 func (server *Server) Election(ctx context.Context, currentCandidate *proto.RingLeaderTopDawgG) (*proto.Acknowledgement, error) {
-	//check for own identifier
-	if currentCandidate.ProcessID == int32(server.port) {
-		AnnounceResult(currentCandidate)
-		return &proto.Acknowledgement{}, nil
-	}
+	isElecting = true
 
-	//compare with own bid and identifier
-	if currentCandidate.Bid < bid || (currentCandidate.Bid == bid && currentCandidate.ProcessID < int32(server.port)) {
-		currentCandidate.Bid = bid
-		currentCandidate.ProcessID = int32(server.port)
-	}
+	go func() {
+		//check for own identifier
+		if currentCandidate.ProcessID == int32(server.port) {
+			AnnounceResult(currentCandidate)
+			leader = server.port // set yourself as leader
+			return
+		}
 
-	//pass along to neighbour
-	neighbour := servers[8080+((*serverId-1)%3)]
-	neighbour.Election(context.Background(), currentCandidate)
+		//compare with own bid and identifier
+		if currentCandidate.Bid < bid || (currentCandidate.Bid == bid && currentCandidate.ProcessID < int32(server.port)) {
+			currentCandidate.Bid = bid
+			currentCandidate.ProcessID = int32(server.port)
+		}
 
-	//wait for timeout
-
-	//pass further if timeout
+		PassElection(ctx, currentCandidate)
+	}()
 
 	return &proto.Acknowledgement{}, nil
+}
+
+func PassElection(ctx context.Context, currentCandidate *proto.RingLeaderTopDawgG) {
+	for i := 0; i < 2; i++ { //try to contact 2 subsequent nodes in ring. Could be modified to loop over all nodes
+		neighbour := servers[8080+((*serverId+i)%3)]
+		fmt.Println("neighbour:", 8080+((*serverId+i)%3))
+
+		deadlineContext, cancel := context.WithDeadline(context.Background(), time.Now().Add(timeout))
+		defer cancel()
+		neighbour.Election(deadlineContext, currentCandidate)
+
+		// Check for timeout reached
+		if deadlineContext.Err() == nil {
+			break
+		}
+		// pass to next neighbour if timeout
+	}
 }
 
 func AnnounceResult(currentCandidate *proto.RingLeaderTopDawgG) {
 	for _, server := range servers {
 		server.Elected(context.Background(), currentCandidate)
 	}
+}
+
+func (server *Server) Elected(ctx context.Context, electedLeader *proto.RingLeaderTopDawgG) (*proto.Acknowledgement, error) {
+	fmt.Println(strconv.Itoa(int(electedLeader.ProcessID)) + " has been elected!")
+	leader = int(electedLeader.ProcessID)
+	isElecting = false
+	return &proto.Acknowledgement{}, nil
+}
+
+func (server *Server) WhoIsNewLeader(ctx context.Context, void *proto.Empty) (*proto.NewPrimary, error) {
+	if !isElecting {
+		isElecting = true
+		PassElection(ctx, &proto.RingLeaderTopDawgG{
+			ProcessID: int32(server.port),
+			Bid:       bid,
+		})
+	}
+
+	// wait for election to finish
+	for isElecting {
+		time.Sleep(5 * time.Second)
+	}
+
+	return &proto.NewPrimary{
+		Port: int32(leader),
+	}, nil
+}
+
+func (server *Server) Bid(context.Context, *proto.ClientBid) (*proto.Acknowledgement, error) {
+	fmt.Println("Bid has been called!")
+	return &proto.Acknowledgement{}, nil
+}
+
+func (server *Server) UpdateBid(context.Context, *proto.ServerBid) (*proto.Acknowledgement, error) {
+	return &proto.Acknowledgement{}, nil
+}
+
+func (server *Server) Result(context.Context, *proto.Empty) (*proto.AuctionStatus, error) {
+	return nil, nil // todo: needs to return auction status
 }
 
 func connectToServer(port int) {
